95adbd89f22bab2a9c1cff2154c19352330f12be
Yohou Tehe <yohou.tehe@telecomnancy.eu>
2022-12-25 15:27:58 +0100

    controles sémantiques

--- src/tds/tdsVisitor.java
+++ src/tds/tdsVisitor.java
@@ -57,13 +57,15 @@ import ast.Divide;
 
 public class tdsVisitor implements AstVisitor<String> {
 
-    private ArrayList<Tds> tdsGlobal; //La liste de toutes les TDS
-    private Stack pileRO;  // La pile des régions ouvertes
+    private ArrayList<Tds> tdsGlobal; // La liste de toutes les TDS
+    private Stack pileRO; // La pile des régions ouvertes
     private TDS currentTDS;
 
     @Override
     public String visit(RecField affect) {
-        //Controle sémantique de type
+        // Field names, expression types, and the order thereof must exactly
+        // match those of the given record type.
+
     }
 
     @Override
@@ -80,6 +82,7 @@ public class tdsVisitor implements AstVisitor<String> {
 
     @Override
     public String visit(LetInEnd dec) {
+
         currentTDS.add(/* entry */)
         TDS tds = new TDS(/*region*/, /*imbrication*/);
         //change currentTDS
@@ -112,46 +115,67 @@ public class tdsVisitor implements AstVisitor<String> {
 
     @Override
     public String visit(LvalueSub dec) {
-
+        // l'index expr doit de type int
+        // l'identifiant de la lvalue doit être de type array
+        // le résultat doit avoit le même type que les éléments de la lvalue qui est de
+        // type array
     }
 
     @Override
     public String visit(ArrayCreate a) {
+        // The type of the Id must refer to an array type.
+        // The expression in square brackets must be int, and
+        // the expression after of must match the element
+        // type of the array. The result type is the array type.
 
     }
 
     @Override
     public String visit(RecCreate a) {
+        // The tyId(type of the id) must refer to a record type,
+        // and the order, names, and types of fields must
+        // match. The result type is the record type.
 
-    }   
+    }
 
     @Override
     public String visit(For f) {
+        // The start and end index must be of type int. The variable is of type int and
+        // must not be
+        // assigned to in the body. The body must be of type
+        // void. The result type is void.
 
     }
 
     @Override
     public String visit(While d) {
+        // The condition type must be int, and the
+        // body type must be void. The result type is void.
 
     }
 
     @Override
     public String visit(LvalueAffect d) {
+        // l'identifiant et l'expression doivent avoir le même type
 
     }
 
     @Override
-    public String visit(StrNode d) { 
+    public String visit(StrNode d) {
 
     }
 
     @Override
     public String visit(ArrayTypeNode n) {
-        
+
     }
 
     @Override
     public String visit(CallExpr d) {
+        // The identifier must refer to a function.
+        // The number and types of actual and formal parameters must be the same. The
+        // type of the call is
+        // the return type of the function
 
     }
 
@@ -162,86 +186,109 @@ public class tdsVisitor implements AstVisitor<String> {
 
     @Override
     public String visit(Precedence_1 mult) {
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(Precedence_2 plus) {
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(Precedence_3 equal) {
+        // les opérands doivent avoir le même type et le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Precedence_4 and) {
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(Or or) {
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(Sup_equal supeq) {
+        // les opérands doivent avoir le même type et le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Inf_equal infeq) {
+        // les opérands doivent avoir le même type et doivent être de type int ou de
+        // type string
+        // le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Superior sup) {
+        // les opérands doivent avoir le même type et doivent être de type int ou de
+        // type string
+        // le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Inferior inf) {
+        // les opérands doivent avoir le même type et doivent être de type int ou de
+        // type string
+        // le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Sup_inf sinf) {
+        // les opérands doivent avoir le même type et doivent être de type int ou de
+        // type string
+        // le résultat doit être de type int
 
     }
 
     @Override
     public String visit(Negate_instruction Ni) {
+        // l'opérand et le résultat sont de type int
 
     }
 
     @Override
     public String visit(IfThen ifThen) {
-
+        // The condition type must be int, and the
+        // then-clause must be of type void. The result type
+        // is also void
 
     }
 
     @Override
     public String visit(IfThenElse ifThenElse) {
-
+        // The condition type must be int, and
+        // the then-clause and else-clause must have the same
+        // type, which becomes the result type
 
     }
 
     @Override
     public String visit(Divide divide) {
-        
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(ExprSeq exprseq) {
-
+        // If the sequence is empty, the type is void,
+        // otherwise, the type is that of the last expression
 
     }
 
     @Override
     public String visit(ExprList exprlist) {
-
 
     }
 
@@ -252,12 +299,14 @@ public class tdsVisitor implements AstVisitor<String> {
 
     @Override
     public String visit(RecFieldList recFieldList) {
-
+        // Field names, expression types, and the order thereof must exactly
+        // match those of the given record type.
 
     }
 
     @Override
     public String visit(LvalueInit lvalueInit) {
+        // l'identifiant doit référencer une variable
 
     }
 
@@ -268,12 +317,13 @@ public class tdsVisitor implements AstVisitor<String> {
 
     @Override
     public String visit(Minus minus) {
-
+        // les opérands et le résultat doivent être de type int
 
     }
 
     @Override
     public String visit(Print print) {
+        // l'argument doit être de type string
 
     }
 
